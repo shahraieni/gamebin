45d17bc663f48d6f2e8782a29b099239a9aaccc2
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2026-07-04 07:55:01 +0330

    a

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, OnInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.css'
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements OnInit, AfterContentInit {
 
+
+
+  @ContentChildren(TabComponent) tabs:QueryList<TabComponent> = new QueryList<TabComponent> ();
+
+  ngOnInit(): void {
+   
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
+  }
 }

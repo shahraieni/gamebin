f1ef158d78f4d0bffb59c5d2e9d37fd010a476a2
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2026-07-04 16:25:09 +0330

    as

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -17,6 +17,17 @@ export class TabsContainerComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit(): void {
-    console.log(this.tabs);
+    const activeTabs = this.tabs.filter(tab => tab.active);
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs.forEach(tab => {
+      tab.active = false;
+    });
+    tab.active = true;
+    return false;
   }
 }

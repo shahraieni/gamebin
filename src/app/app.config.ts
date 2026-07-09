b81eb008e9449a2c7c2d7cac5c51d18d57e8f420
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2026-07-09 10:14:42 +0330

    firebaseConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+  ],
+  
+    
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyD4x5iET2yEEpOt-J7ZeJrAT9WYVnmZscs",
+        authDomain: "gamebin.firebaseapp.com",
+        projectId: "gamebin",
+        storageBucket: "gamebin.firebasestorage.app",
+        messagingSenderId: "568873115546",
+        appId: "1:568873115546:web:69b424c988fb17dace6423",
+        measurementId: "G-28VBCNDXX9"
+    }
+};

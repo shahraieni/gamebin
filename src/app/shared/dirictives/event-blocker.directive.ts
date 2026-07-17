331f53f2e34b5e720d6b325947794ca5b3f57577
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2026-07-17 18:49:38 +0330

    EventBlockerDirective

---
+++ src/app/shared/dirictives/event-blocker.directive.ts
@@ -0,0 +1,18 @@
+import { Directive , HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[app-event-blocker]',
+  standalone: true
+})
+export class EventBlockerDirective {
+
+  constructor() { }
+
+  @HostListener('drop',['$event'])
+  @HostListener('dragover',['$event'])
+
+  public handlerEvent(event:Event){
+      event.preventDefault();
+  }
+
+}
